59a56d08750b63322a3b9d573d380ac1ccc25b5a
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2023-11-26 14:24:09 +0530

    first commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle login logic here
+  };
+
+  return (
+    <div className="container mt-5 bg-dark">
+      <h2>MoneymantraAI Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input 
+            type="email" 
+            className="form-control" 
+            id="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+            required 
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input 
+            type="password" 
+            className="form-control" 
+            id="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            required 
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+        <div className="mt-3">
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/components/SignUpPage.js
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle sign-up logic here
+    if (password !== confirmPassword) {
+      // Display an error message or handle the mismatch
+      alert("Passwords do not match.");
+      return;
+    }
+    const userData = {
+      fullName,
+      email,
+      phoneNumber,
+      password,
+      role,
+    };
+    console.log(
+      "userData================================================>",
+      userData
+    );
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="fullName" className="form-label">
+            Full Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="fullName"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <div className="mb-3">
+            <label htmlFor="phoneNumber" className="form-label">
+              Phone Number
+            </label>
+            <input
+              type="tel"
+              className="form-control"
+              id="phoneNumber"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="role" className="form-label">
+            Role
+          </label>
+          <select
+            className="form-select"
+            id="role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            required
+          >
+            <option value="">Select a role</option>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </select>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+        <div className="mt-3">
+          Already have an account? <Link to="/login">Login</Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpPage;
