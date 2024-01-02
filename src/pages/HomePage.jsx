ae07bbe7fb4f395a4c2460c03af069f857d17d0a
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-01-02 10:55:15 +0530

    design change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -30,80 +30,77 @@ function HomePage() {
     <div
       style={{
         height: "100vh",
-   
       }}
       //className="container-fluid"
     >
-      
-        {/* Navigation Bar */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <Link to="/" className="navbar-brand">
-              <img
-                src={require("../assets/images/mmremovebg.png")}
-                alt="logo"
-                width="100"
-              />
-            </Link>
-            <div>
-              <h1>Moneymantra AI</h1>
-            </div>
-            <div className="d-flex">
-              <Link to="/login" className="btn btn-outline-secondary me-2">
-                Login
-              </Link>
-              <Link to="/signup" className="btn btn-primary">
-                Sign Up
-              </Link>
-            </div>
-          </div>
-        </nav>
-        <div className="row bg-danger">
-          <div className="col-lg-6 p-5 gradient_background_reverse d-flex flex-column justify-content-center">
-            <h1>Strategize Wealth, Maximize Growth.</h1>
-
-            <p>
-              Money Mantra is a cutting-edge financial platform dedicated to
-              empowering investors with smart, data-driven strategies for the
-              stock market. At the heart of our mission lies a commitment to
-              democratizing financial success through the use of advanced
-              artificial intelligence. We provide both novice traders and
-              seasoned investors with the tools they need to make informed
-              decisions, minimize risk, and capitalize on growth opportunities.
-              With Money Mantra, you gain a trusted partner in your financial
-              journey, ensuring that every move you make is backed by expertise
-              and precision, paving your path to financial prosperity.
-            </p>
-
-            <Link to="/signup" className="btn btn-light">
-              Sign Up for free!
-            </Link>
-          </div>
-          <div className="col-lg-6 d-flex flex-column align-items-center gradient_background justify-content-center ">
+      {/* Navigation Bar */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
             <img
-              src={require("../assets/images/homeimage.png")}
+              src={require("../assets/images/mmremovebg.png")}
               alt="logo"
-              width="90%"
+              width="100"
             />
+          </Link>
+          <div>
+            <h1>Moneymantra AI</h1>
+          </div>
+          <div className="d-flex">
+            <Link to="/login" className="btn btn-outline-secondary me-2">
+              Login
+            </Link>
+            <Link to="/signup" className="btn btn-primary">
+              Sign Up
+            </Link>
           </div>
         </div>
-        <Footer />
+      </nav>
+      <div className="row bg-danger">
+        <div className="col-lg-6 p-5 gradient_background_reverse d-flex flex-column justify-content-center">
+          <h1 className="neon-text">Strategize Wealth, Maximize Growth.</h1>
+
+          <p className="neon-text-yellow">
+            Money Mantra is a cutting-edge financial platform dedicated to
+            empowering investors with smart, data-driven strategies for the
+            stock market. At the heart of our mission lies a commitment to
+            democratizing financial success through the use of advanced
+            artificial intelligence. We provide both novice traders and seasoned
+            investors with the tools they need to make informed decisions,
+            minimize risk, and capitalize on growth opportunities. With Money
+            Mantra, you gain a trusted partner in your financial journey,
+            ensuring that every move you make is backed by expertise and
+            precision, paving your path to financial prosperity.
+          </p>
+
+          <Link to="/signup" className="btn btn-light">
+            Sign Up for free!
+          </Link>
+        </div>
+        <div className="col-lg-6 d-flex flex-column align-items-center gradient_background justify-content-center ">
+          <img
+            src={require("../assets/images/rupeegold.png")}
+            alt="logo"
+            width="90%"
+          />
+        </div>
+      </div>
+      <Footer />
+      <div
+        className="text-center text-lg-start bg-light text-muted "
+        style={{
+          backgroundColor: "pink",
+
+          borderTop: "1px solid #e7e7e7",
+        }}
+      >
         <div
-          className="text-center text-lg-start bg-light text-muted "
-          style={{
-            backgroundColor: "pink",
-          
-            borderTop: "1px solid #e7e7e7",
-          }}
+          className="text-center p-4"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
         >
-          <div
-            className="text-center p-4"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-          >
-            © 2023 Moneymantra AI. All rights reserved.
-          </div>
+          © 2023 Moneymantra AI. All rights reserved.
         </div>
-   
+      </div>
     </div>
   );
 }
